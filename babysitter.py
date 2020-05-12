607a204e589dd13ee034c6d9fc95c7e4f5e51ffa
Christian Cumberlander <christiancumberlander@ChristiansMBP4.attlocal.net>
2020-05-12 19:54:01 -0400

    Accounts for midnight. validates end time

--- babysitter.py
+++ babysitter.py
@@ -12,7 +12,9 @@
 # The time ranges are the same as the babysitter (5pm through 4am)
 
 def hours_worked(start_time, end_time):
-    if start_time < 5:
-        start_time = 5
+    if start_time < 17:
+        start_time = 17
+    if end_time not in range(12,24 + 1):
+        end_time += 24
     return end_time - start_time
 
--- test_babysitter.py
+++ test_babysitter.py
@@ -1,7 +1,10 @@
 from babysitter import *
 
 def test_hours_worked():
-    assert hours_worked(5,6) == 1
+    assert hours_worked(17,18) == 1
 
 def test_accurate_start_time():
-    assert hours_worked(4,6) == 1
+    assert hours_worked(16,18) == 1
+
+def test_accurate_end_time():
+    assert hours_worked(23,0) == 1  
