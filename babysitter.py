7e6dd3729d0d3a42ae118e160d060b6039497d0c
Christian Cumberlander <christiancumberlander@ChristiansMBP4.attlocal.net>
2020-05-12 20:02:19 -0400

    Refectored

--- babysitter.py
+++ babysitter.py
@@ -12,9 +12,18 @@
 # The time ranges are the same as the babysitter (5pm through 4am)
 
 def hours_worked(start_time, end_time):
+    check_start_time(start_time):
+    def check_end_time(end_time):
+    return end_time - start_time
+
+def check_start_time(start_time):
     if start_time < 17:
         start_time = 17
+    return start_time
+def check_end_time(end_time):
     if end_time not in range(12,24 + 1):
         end_time += 24
-    return end_time - start_time
+    return end_time
+        
+
 
